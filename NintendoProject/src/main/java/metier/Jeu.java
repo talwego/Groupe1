e7da7124086ccba89734d9835c0928c365ff8f00
Fabian Polvin <polvin.fabian@gmail.com>
2022-10-10 15:00:40 +0200

    Auto stash before merge of "main" and "origin/main"
    Jeu => add Boutique

--- NintendoProject/src/main/java/metier/Jeu.java
+++ NintendoProject/src/main/java/metier/Jeu.java
@@ -6,44 +6,39 @@ public class Jeu {
 
 	private String titre;
 	private Console console;
-	
-	
-	
-	public Jeu(String titre, Console console) {
+	private Boutique boutique;
+	public Jeu(String titre, Console console, Boutique boutique) {
+		super();
 		this.titre = titre;
 		this.console = console;
+		this.boutique = boutique;
 	}
-
-
-
 	public String getTitre() {
 		return titre;
 	}
-
-
-
 	public void setTitre(String titre) {
 		this.titre = titre;
 	}
-
-
-
 	public Console getConsole() {
 		return console;
 	}
-
-
-
 	public void setConsole(Console console) {
 		this.console = console;
 	}
-
-
-
+	public Boutique getBoutique() {
+		return boutique;
+	}
+	public void setBoutique(Boutique boutique) {
+		this.boutique = boutique;
+	}
 	@Override
 	public String toString() {
-		return "Jeu [titre=" + titre + ", console=" + console + "]";
+		return "Jeu [titre=" + titre + ", console=" + console + ", boutique=" + boutique + "]";
 	}
+	
+	
+	
+	
 	
 	
 }
