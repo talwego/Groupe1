5d3ed4a8c406ffb4cd4bcdaea653042bc3c73812
Fabian Polvin <polvin.fabian@gmail.com>
2022-10-10 15:27:42 +0200

    Auto stash before merge of "main" and "origin/main"
    modif console

--- NintendoProject/src/main/java/metier/Console.java
+++ NintendoProject/src/main/java/metier/Console.java
@@ -1,63 +1,41 @@
 package metier;
 
-<<<<<<< HEAD
 import java.time.LocalDate;
 
-public class Console {
-
-	
+public abstract class Console {
 
 	private String nom;
 	private int prix;
 	private LocalDate sortie;
-	
 	public Console(String nom, int prix, LocalDate sortie) {
 		super();
 		this.nom = nom;
 		this.prix = prix;
 		this.sortie = sortie;
 	}
-=======
-public abstract class Console {
-
-	protected String nom;
->>>>>>> main
-
-	public Console(String nom) {
-		this.nom = nom;
-	}
-
 	public String getNom() {
 		return nom;
 	}
-
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-
-	@Override
-	public String toString() {
-		return "Console [nom=" + nom + "]";
-	}
-<<<<<<< HEAD
-
 	public int getPrix() {
 		return prix;
 	}
-
 	public void setPrix(int prix) {
 		this.prix = prix;
 	}
-
 	public LocalDate getSortie() {
 		return sortie;
 	}
-
 	public void setSortie(LocalDate sortie) {
 		this.sortie = sortie;
 	}
+	@Override
+	public String toString() {
+		return "Console [nom=" + nom + ", prix=" + prix + ", sortie=" + sortie + "]";
+	}
 	
 	
-=======
->>>>>>> main
+	
 }
