88e7993b1c949c7d408c1dc1a310fa6066782a30
talwego <talweg42@proton.me>
2022-10-10 15:15:35 +0200

    jeux

--- NintendoProject/src/test/java/test/Test.java
+++ NintendoProject/src/test/java/test/Test.java
@@ -1,10 +1,16 @@
 package test;
 
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 import metier.Client;
 =======
 import metier.Console;
 >>>>>>> main
+=======
+import metier.Boutique;
+import metier.Console;
+import metier.Jeu;
+>>>>>>> Stashed changes
 
 public class Test {
 
@@ -25,6 +31,19 @@ public class Test {
 		Console c3 = new Console ("Ps1");
 		Console c4 = new Console ("Ps3");
 		Console c5 = new Console ("Ps4");
+		
+		Boutique b1 = new Boutique("la boutique", "5 rue des alouettes");
+		
+		
+		Jeu j1 = new Jeu ("Le retour de la raclette", c1, b1);
+		Jeu j2 = new Jeu ("Le retour de l'hivert", c1, b1);
+		Jeu j3 = new Jeu ("Le retour du vin chaud", c1, b1);
+		Jeu j4 = new Jeu ("Le retour de la crève", c1, b1);
+		Jeu j5 = new Jeu ("Le retour des vagues", c1, b1);
+
+
+		
+		
 		
 		// TODO Auto-generated method stub
 
